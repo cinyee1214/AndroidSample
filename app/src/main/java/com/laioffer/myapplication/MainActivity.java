cf644c5f586001f7190f526045e183f6dab0faa8
cinyee1214 <xinyizhao1214@gmail.com>
2020-10-05 23:37:23 -0400

    import

--- app/src/main/java/com/laioffer/myapplication/MainActivity.java
+++ app/src/main/java/com/laioffer/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.laioffer.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
